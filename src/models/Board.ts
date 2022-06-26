06b3c3605269dbea67167a63adbd74fe108a3794
vladislavorehov55 <vladislavorehov55@gmail.com>
2022-06-27 00:19:09 +0300

    Add: opportunity to move Queen

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -58,7 +58,7 @@ export class Board {
         this.cells[7][i].figure = new Queen(Colors.WHITE);
       }
     }
-    this.cells[3][1].figure = new Rook(Colors.BLACK)
+    this.cells[3][1].figure = new Queen(Colors.BLACK)
     // this.cells[3][1].figure = new Pawn(Colors.BLACK)
   }
   getBoardCopy() {
--- src/models/figures/Queen.ts
+++ src/models/figures/Queen.ts
@@ -15,25 +15,34 @@ export class Queen extends Figures {
     let isContinueCheckSecondDiag = true;
     let isContinueCheckThirdDiag = true;
     let isContinueCheckFourthDiag = true;
-    let isContinueCheckVerticalLine = true;
-    let isContinueCheckHorizontalLine = true;
+    let isContinueUpperLine = true;
+    let isContinueCheckRightLine = true;
+    let isContinueCheckLowerLine = true;
+    let isContinueCheckLeftLine = true;
 
-    for (let moveStep = 1, moveStep2 = 0; moveStep < 8; moveStep++, moveStep2++) {
+    for (let moveStep = 1; moveStep <= 7; moveStep++) {
       if (isContinueCheckFirstDiag) {
         if (cells[x - moveStep]?.[y - moveStep]?.figure === undefined) {
         } else if (cells[x - moveStep]?.[y - moveStep]?.figure === null) {
           cells[x - moveStep][y - moveStep].isAvailableForMove = true;
-        } else if (cells[x - moveStep][y - moveStep].figure && cells[x - moveStep][y - moveStep].figure?.color !== this.color) {
+        }
+        else if (cells[x - moveStep][y - moveStep].figure && cells[x - moveStep][y - moveStep].figure?.color === this.color) {
+          isContinueCheckFirstDiag = false;
+        }
+        else if (cells[x - moveStep][y - moveStep].figure && cells[x - moveStep][y - moveStep].figure?.color !== this.color) {
           isContinueCheckFirstDiag = false;
           cells[x - moveStep][y - moveStep].isAvailableForMove = true;
-
         }
       }
       if (isContinueCheckSecondDiag) {
         if (cells[x - moveStep]?.[y + moveStep]?.figure === undefined) {
         } else if (cells[x - moveStep][y + moveStep].figure === null) {
           cells[x - moveStep][y + moveStep].isAvailableForMove = true;
-        } else if (cells[x - moveStep][y + moveStep].figure && cells[x - moveStep][y + moveStep].figure?.color !== this.color) {
+        }
+        else if (cells[x - moveStep][y + moveStep].figure && cells[x - moveStep][y + moveStep].figure?.color === this.color) {
+          isContinueCheckSecondDiag = false;
+        }
+        else if (cells[x - moveStep][y + moveStep].figure && cells[x - moveStep][y + moveStep].figure?.color !== this.color) {
           isContinueCheckSecondDiag = false;
           cells[x - moveStep][y + moveStep].isAvailableForMove = true;
         }
@@ -42,7 +51,11 @@ export class Queen extends Figures {
         if (cells[x + moveStep]?.[y + moveStep]?.figure === undefined) {
         } else if (cells[x + moveStep][y + moveStep].figure === null) {
           cells[x + moveStep][y + moveStep].isAvailableForMove = true;
-        } else if (cells[x + moveStep][y + moveStep].figure && cells[x + moveStep][y + moveStep].figure?.color !== this.color) {
+        }
+        else if (cells[x + moveStep][y + moveStep].figure && cells[x + moveStep][y + moveStep].figure?.color === this.color) {
+          isContinueCheckThirdDiag = false;
+        }
+        else if (cells[x + moveStep][y + moveStep].figure && cells[x + moveStep][y + moveStep].figure?.color !== this.color) {
           isContinueCheckThirdDiag = false;
           cells[x + moveStep][y + moveStep].isAvailableForMove = true;
         }
@@ -52,31 +65,56 @@ export class Queen extends Figures {
         } else if (cells[x + moveStep][y - moveStep].figure === null) {
           cells[x + moveStep][y - moveStep].isAvailableForMove = true;
         }
+        else if (cells[x + moveStep][y - moveStep].figure && cells[x + moveStep][y - moveStep].figure?.color === this.color) {
+          isContinueCheckFourthDiag = false;
+        }
         else if (cells[x + moveStep][y - moveStep].figure && cells[x + moveStep][y - moveStep].figure?.color !== this.color) {
           isContinueCheckFourthDiag = false;
           cells[x + moveStep][y - moveStep].isAvailableForMove = true;
         }
       }
-      if (isContinueCheckHorizontalLine) {
-        if (cells[x + moveStep2][y].figure === null) {
-          cells[x + moveStep2][y].isAvailableForMove = true;
-        }
-        else {
-          if (cells[x + moveStep2][y].figure?.color !== this.color) {
-            cells[x + moveStep2][y].isAvailableForMove = true;
-            isContinueCheckHorizontalLine = false;
-          }
+      if (isContinueUpperLine) {
+        if (cells[x - moveStep]?.[y]?.figure === undefined) {
+        } else if (cells[x - moveStep][y].figure === null) {
+          cells[x - moveStep][y].isAvailableForMove = true;
+        } else if (cells[x - moveStep][y].figure && cells[x - moveStep][y].figure?.color === this.color) {
+          isContinueUpperLine = false;
+        } else if (cells[x - moveStep][y].figure && cells[x - moveStep][y].figure?.color !== this.color) {
+          isContinueUpperLine = false;
+          cells[x - moveStep][y].isAvailableForMove = true;
         }
       }
-      if (isContinueCheckVerticalLine) {
-        if (cells[x][y + moveStep2].figure === null) {
-          cells[x][y + moveStep2].isAvailableForMove = true;
+      if (isContinueCheckRightLine) {
+        if (cells[x]?.[y + moveStep]?.figure === undefined) {
+        } else if (cells[x][y + moveStep].figure === null) {
+          cells[x][y + moveStep].isAvailableForMove = true;
+        } else if (cells[x][y + moveStep].figure && cells[x][y + moveStep].figure?.color === this.color) {
+          isContinueCheckRightLine = false;
+        } else if (cells[x][y + moveStep].figure && cells[x][y + moveStep].figure?.color !== this.color) {
+          isContinueCheckRightLine = false;
+          cells[x][y + moveStep].isAvailableForMove = true;
         }
-        else {
-          if (cells[x][y + moveStep2].figure?.color !== this.color) {
-            cells[x][y + moveStep2].isAvailableForMove = true;
-            isContinueCheckVerticalLine = false;
-          }
+      }
+      if (isContinueCheckLowerLine) {
+        if (cells[x + moveStep]?.[y]?.figure === undefined) {
+        } else if (cells[x + moveStep][y].figure === null) {
+          cells[x + moveStep][y].isAvailableForMove = true;
+        } else if (cells[x + moveStep][y].figure && cells[x + moveStep][y].figure?.color === this.color) {
+          isContinueCheckLowerLine = false;
+        } else if (cells[x + moveStep][y].figure && cells[x + moveStep][y].figure?.color !== this.color) {
+          isContinueCheckLowerLine = false;
+          cells[x + moveStep][y].isAvailableForMove = true;
+        }
+      }
+      if (isContinueCheckLeftLine) {
+        if (cells[x]?.[y - moveStep]?.figure === undefined) {
+        } else if (cells[x][y - moveStep].figure === null) {
+          cells[x][y - moveStep].isAvailableForMove = true;
+        } else if (cells[x][y - moveStep].figure && cells[x][y - moveStep].figure?.color === this.color) {
+          isContinueCheckLeftLine = false;
+        } else if (cells[x][y - moveStep].figure && cells[x][y - moveStep].figure?.color !== this.color) {
+          isContinueCheckLeftLine = false;
+          cells[x][y - moveStep].isAvailableForMove = true;
         }
       }
     }
