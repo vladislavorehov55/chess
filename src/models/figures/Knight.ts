5ead69b539446cadf8f58156347f05768aba6a20
vladislavorehov55 <vladislavorehov55@gmail.com>
2022-06-26 22:16:07 +0300

    Add: opportunity to move Knight

--- src/models/figures/Knight.ts
+++ src/models/figures/Knight.ts
@@ -9,4 +9,17 @@ export class Knight extends Figures {
   constructor(color: Colors) {
     super(FiguresNames.KNIGHT, color, color === Colors.WHITE ? whiteKnightLogo : blackKnightLogo);
   }
+
+  canMove(board: Board, x: number, y: number) {
+    const cells: Cell[][] = board.cells;
+    const moveSteps = [[-2, -1], [-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2]];
+    for (let [stepX, stepY] of moveSteps) {
+      if (cells[x + stepX]?.[y + stepY]?.figure === undefined) {
+      } else if (cells[x + stepX]?.[y + stepY]?.figure === null) {
+        cells[x + stepX][y + stepY].isAvailableForMove = true;
+      } else if (cells[x + stepX][y + stepY].figure && cells[x + stepX][y + stepY].figure?.color !== this.color) {
+        cells[x + stepX][y + stepY].isAvailableForMove = true;
+      }
+    }
+  }
 }
